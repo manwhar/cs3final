874486abec72dee913b1e3a726e1236bc9fb4c90
manu <812194@seq.org>
2024-12-19 17:37:10 -0800

    Final rendition!

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const tabs = document.querySelectorAll('.tab');
     const tabContents = document.querySelectorAll('.tab-content');
